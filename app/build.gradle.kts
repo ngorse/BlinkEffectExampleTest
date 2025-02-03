158569025463bfc8caed15a86bd8e2ddca2f02d3
Nico Gorse <nico@secretsaucepartners.com>
2025-02-03 19:43:50 +0100

    Full test using v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.ngorse:BlinkEffectExample:v1.0")
+    implementation("com.github.ngorse:BlinkEffectExample:v1.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
